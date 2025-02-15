d1c96abc9f9a1a9b82c0da8841718dbb5a60d409
Dmytro Parzhytskyi <parzhitsky@gmail.com>
2025-02-15 17:23:20 +0200

    Put context creation out of the entry file

--- packages/playground/src/example/index.ts
+++ packages/playground/src/example/index.ts
@@ -1,17 +1,5 @@
-import { Context } from '@/di/context.js'
-import { Server } from './server.js'
 import { App } from './app.js'
-import { Config } from './config.js'
-import { Logger } from './logger.js'
-
-const context = new Context()
-  .register(Config)
-  .register(Logger)
-  .register(Server)
-  .inject(Server, 0, Logger)
-  .register(App)
-  .inject(App, 0, Config)
-  .inject(App, 1, Server)
+import { context } from './context.js'
 
 const app = await context.instantiate(App)
 
